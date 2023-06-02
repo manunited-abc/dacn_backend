a7ac579798d460420ce307636345511bc1bcb162
khaihieu <khaihieupc2@gmail.com>
2023-06-02 11:32:09 +0700

    spring zero

---
+++ src/main/java/nlu/dacn/dacn_backend/DacnBackendApplication.java
@@ -0,0 +1,13 @@
+package nlu.dacn.dacn_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DacnBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nlu/dacn/dacn_backend/DacnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nlu.dacn.dacn_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
